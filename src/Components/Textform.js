28b3005342a8c447e70b9c8c254f09bef5be3998
anshujain164 <anshujain641@gmail.com>
2024-07-19 14:34:16 +0530

    Changes Applied

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -57,7 +57,7 @@ const Textform = (props) => {
       </div>
       <div className="container" style={{color : props.mode === 'dark'?'white': '#042743 '}}>
         <h2>Your Text Summary</h2>
-        <p>{text.split(/\s+/).filter((element)=>{return element.length !== 0}).length} words and {text.length} charaters</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length !== 0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length !== 0}).length} Minutes read</p>
         <h2>Preview</h2> 
         <p>{text.length>0?text:"Nothing to preview!"}</p>
